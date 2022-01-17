88540c081bae3d2443d9d86f4e05f60c460c4c21
Shivammishra789 <shivammishra7789@gmail.com>
2022-01-17 14:02:44 +0530

    ADDED: trigger problems

--- Triggers/trigger.sql
+++ Triggers/trigger.sql
@@ -3,20 +3,20 @@ To update valid password , trigger will be invoked before update to
 check the new value of password if its empty sql error message will be singled. 
 */
 DELIMITER $$
-	CREATE TRIGGER before_update_password
-	BEFORE UPDATE ON user_details FOR EACH ROW
-	BEGIN
-		DECLARE error_msg1 VARCHAR(600);
-        DECLARE error_msg2 VARCHAR(600);
-		SET error_msg1=('Password cannot be empty');
-		SET error_msg2=('New Password Should Be Defferent From Old Password.');
-			IF
-				new.password = '' THEN signal sqlstate '45000' set message_text=error_msg1;
-			ELSEIF
-				new.password = old.password THEN signal sqlstate '44000' set message_text=error_msg2;
-			END IF;
-	END$$
-  DELIMITER ;      
+CREATE TRIGGER before_update_password
+BEFORE UPDATE ON user_details FOR EACH ROW
+BEGIN
+	DECLARE error_msg1 VARCHAR(600);
+	DECLARE error_msg2 VARCHAR(600);
+	SET error_msg1=('Password cannot be empty');
+	SET error_msg2=('New Password Should Be Defferent From Old Password.');
+	IF
+		new.password = '' THEN signal sqlstate '45000' set message_text=error_msg1;
+	ELSEIF
+		new.password = old.password THEN signal sqlstate '44000' set message_text=error_msg2;
+	END IF;
+END$$
+DELIMITER ;      
   
   UPDATE user_details set password = '' where id = 3;
   
@@ -24,14 +24,15 @@ DELIMITER $$
   Delete Trigger:- After deleting a row from company table, trigger will be 
   invoked to insert the company name and date_deleted in another table named as company_trash
   */
-  DELIMITER $$
-	CREATE TRIGGER after_company_delete
-	AFTER DELETE ON company FOR EACH ROW
-	BEGIN
-		INSERT INTO company_trash
-		VALUES (old.name, CURDATE());
-	END$$
-  DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER after_company_delete
+AFTER DELETE ON company FOR EACH ROW
+BEGIN
+	INSERT INTO company_trash
+	VALUES (old.name, CURDATE());
+END$$
+DELIMITER ;
 
 DELETE FROM company where name='TCS';
-  select * from company_trash;
+  
+SELECT * FROM company_trash;
